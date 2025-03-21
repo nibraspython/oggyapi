29a364a76e81dfcd1eb9c34b831303743ed46f97
nibras <brihsan40@gmail.com>
2025-03-21 10:03:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
   res.status(200).json({ 
-    message: "API is working well.",
+    status: "🟢 Online",
+    message: "API is working well.", 
     services: {
       insta_video_download: "/insta - To download Instagram video",
       web_screenshot: "/ss - To get a screenshot of a website"
