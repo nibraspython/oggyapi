f517380f6f9aaba458f443539b149bf981ff7346
nibras <brihsan40@gmail.com>
2025-03-21 10:00:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "API is working well." });
+  res.status(200).json({ 
+    message: "API is working well.",
+    services: {
+      insta_video_download: "/insta - To download Instagram video",
+      web_screenshot: "/ss - To get a screenshot of a website"
+    },
+    coded_by: "@OGGY_WORKSHOP"
+  });
 }
